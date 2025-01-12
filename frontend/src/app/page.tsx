ecb0c8e6c16fcd3cce3791e790ce149dbdde1285
kenttv11 <kenttv11@yandex.ru>
2025-01-13 01:50:15 +0300

    last update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold text-blue-500 mb-4">
-        Hello Tailwind CSS
+        JIVA PAY
       </h1>
       <p className="text-lg bg-gray-100 p-4 rounded-lg">
-        This text should be styled with Tailwind
+        With Love
       </p>
     </main>
   );
