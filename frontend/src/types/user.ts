dfed859acf8ba464b2ab025d190e4f5d622e145a
kenttv11 <kenttv11@yandex.ru>
2025-01-13 21:15:02 +0300

    last update frontend

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,12 +1,15 @@
 export interface User {
-    id: number;
-    email: string;
-    full_name: string;
-    verification_level: string;
-    referral_code: string | null;
-    referrer_id: number | null;
-    created_at: string;
-    updated_at: string;
-    role_id: number;
-    is_superuser: boolean;
-  }
+  id: number;
+  email: string;
+  full_name: string;
+  phone_number?: string;
+  telegram_username?: string;
+  avatar_url?: string;
+  verification_level: string;
+  referral_code: string | null;
+  referrer_id: number | null;
+  created_at: string;
+  updated_at: string;
+  role_id: number;
+  is_superuser: boolean;
+}
