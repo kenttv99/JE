6c9d76da010ea452ce1dc186698595a00792d1c0
kenttv11 <kenttv11@yandex.ru>
2025-01-28 20:52:58 +0300

    new structure frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -62,9 +62,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
           {/* Main Content */}
           <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            <div className="bg-white rounded-lg shadow-sm p-6">
+            {/* <div className="bg-white rounded-lg shadow-sm p-6"> */}
               {children}
-            </div>
+            {/* </div> */}
           </main>
 
           {/* Footer */}
